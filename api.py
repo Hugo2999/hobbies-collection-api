f72b28a3eb36d3e16dc8ed69997f5cbecf37d9d9
Hugo2999 <hugostoso2641@gmail.com>
2024-09-03 09:37:52 -0300

    aula l

--- api.py
+++ api.py
@@ -1,11 +1,24 @@
-from  flask import Flask, jsonify, request
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///livros.db'
+
+db = SQLAlchemy(app)
+
+class Livro(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    autor = db.Column(db.String(100), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
 livros = [
     {'id': 1, 'titulo': '1984', 'autor': 'George Orwell'},
-    {'id': 2, 'titulo': 'O Senhor dos Aneis', 'autor': 'J. R. R. Tlkien'},
-    {'id': 3, 'titulo': 'Prisioneiro de Azkhaban', 'autor': 'J. K. Rowling'}
+    {'id': 2, 'titulo': 'O Senhor dos Anéis', 'autor': 'J. R. R. Tolkien'},
+    {'id': 3, 'titulo': 'O Prisioneiro de Azkaban', 'autor': 'J. K. Rowling'}
 ]
 
 @app.route('/api/livros', methods=['GET'])
@@ -15,9 +28,9 @@ def get_livros():
 @app.route('/api/livros/<int:id>', methods=['GET'])
 def get_livro(id):
     livro = next(
-        (livro for livro in livros if livro['id'] == id), 
+        (livro for livro in livros if livro['id'] == id),
         None
-    )  
+    )
     return jsonify(livro) if livro else ('', 404)
 
 @app.route('/api/livros', methods=['POST'])
